e3500ce9e45db24ddbca5097aab32e2dd498b3ad
ONIGRL <eduardermakov769@gmail.com>
2025-11-07 22:38:56 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        HOOTONE
+import os        ONIGRL
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
