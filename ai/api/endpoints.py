df39fa518b08278825746c0748a8bfc0fac8172d
FUNPAY <eduardermakov769@gmail.com>
2025-11-07 23:49:03 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        ONIGRL
+import os        FUNPAY
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
